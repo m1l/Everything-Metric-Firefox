c40ea1282c361bb4eb221501d46602c042b30529
Quentin Santos <qsantos@qsantos.fr>
2023-09-24 09:43:59 +0200

    Rename BracketsCheck to isAlreadyConverted

--- contentscript.js
+++ contentscript.js
@@ -267,7 +267,7 @@ function replaceFahrenheit(text) {
         while ((matches = regex.exec(text)) !== null) {
             try {
                 //console.log(matches[0]);
-                if (BracketsCheck(matches[0])) continue;
+                if (isAlreadyConverted(matches[0])) continue;
                 const fullMatch = matches[1];
 
                 //for (var i=0; i<matches.length; i++)
@@ -341,7 +341,7 @@ function mpg2Lper100km(text) {
         while ((matches = regex.exec(text)) !== null) {
             try {
                 //console.log(matches[0]);
-                if (BracketsCheck(matches[0])) continue;
+                if (isAlreadyConverted(matches[0])) continue;
                 const fullMatch = matches[1];
 
                 //for (var i=0; i<matches.length; i++)
@@ -390,7 +390,7 @@ function CleanReplace(text, match, metStr) {
 }
 
 
-function BracketsCheck(text){
+function isAlreadyConverted(text){
     if ((convertBracketed && /[\(]/.test(text.substring(1))) ||
        (!convertBracketed && /[\(\)]/.test(text)) ||
        /\u3010/.test(text))
@@ -411,7 +411,7 @@ function processAll(text) {
 
                     //includes a bracket, it is probably already converted. ex: 1 in (2.54 cm)
 
-                    if (BracketsCheck(matches[0])) continue;
+                    if (isAlreadyConverted(matches[0])) continue;
 
                     if ((matches[2] !== undefined) && (/(?:^|\s)([-−]?\d*\.?\d+|\d{1,3}(?:,\d{3})*(?:\.\d+)?)(?!\S)/g.test(matches[2]) === false)) continue;
 
@@ -592,7 +592,7 @@ function AxAxAin(text) {
         while ((matches = regex.exec(text)) !== null) {
             try {
                 const fullMatch = matches[1];
-                if (BracketsCheck(matches[0])) continue;
+                if (isAlreadyConverted(matches[0])) continue;
 
                 let scale = 2.54;
                 let unit = spc + "cm";
@@ -634,7 +634,7 @@ function AxAqq(text) {//ikea US
             for (var i=0; i<matches.length; i++)
                 console.log("matches " + i + " " + matches[i])*/
             const fullMatch = matches[1];
-            //if (BracketsCheck(text)) continue;
+            //if (isAlreadyConverted(text)) continue;
 
 
             let inches1 = parseFloat(matches[3]);
@@ -695,7 +695,7 @@ function AxAin(text) {
                 const fullMatch = matches[1];
                 if (/[0-9][Xx\*×][ \u00A0][0-9]/.test(fullMatch))
                     continue; //it is 2x 2in something so no conversion
-                if (BracketsCheck(matches[0])) continue;
+                if (isAlreadyConverted(matches[0])) continue;
 
                 let scale = 2.54;
                 let unit = spc + "cm";
@@ -733,7 +733,7 @@ function AxAft(text) {
                 const fullMatch = matches[1];
                 if (/[0-9][xX\*×][ \u00A0][0-9]/.test(fullMatch))
                     continue; //it is 2x 2ft something so no conversion
-                if (BracketsCheck(matches[0])) continue;
+                if (isAlreadyConverted(matches[0])) continue;
 
                 let scale = 0.3048;
                 let unit = spc + "m";
@@ -812,7 +812,7 @@ function feetInch(text) {
             }
 
 
-            if (BracketsCheck(matches[0])) continue;
+            if (isAlreadyConverted(matches[0])) continue;
 
             /*if (/“/.test(fullMatch)) {
                 lastQuoteOpen = true;
