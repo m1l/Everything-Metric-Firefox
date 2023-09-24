133a839324f3f5f8715ba2ab07496ddb20f634a8
Quentin Santos <qsantos@qsantos.fr>
2023-09-24 09:25:21 +0200

    Add tests for stepUpOrDown

--- test.mjs
+++ test.mjs
@@ -1,5 +1,5 @@
 import assert from 'node:assert/strict';
-import { evaluateFraction } from './lib.js';
+import { evaluateFraction, stepUpOrDown } from './lib.js';
 
 async function testEvaluateFraction() {
     assert.equal(evaluateFraction('½'), 0.5);
@@ -10,8 +10,35 @@ async function testEvaluateFraction() {
     assert.equal(evaluateFraction('2 / 1'), 2.0);
 }
 
+async function testStepUpOrDown() {
+    // step down
+    assert.deepEqual(stepUpOrDown(0.125, 'm', false, false), { met: 12.5, unit: 'cm' });
+    assert.deepEqual(stepUpOrDown(0.125, 'm', false, true), { met: 12.5, unit: 'cm' });
+    assert.deepEqual(stepUpOrDown(0.125, 'm', true, false), { met: 125, unit: 'mm' });
+    assert.deepEqual(stepUpOrDown(0.125, 'km', true, false), { met: 125, unit: 'm' });
+
+    // returns 0.125 m instead of 125 mm
+    // assert.deepEqual(stepUpOrDown(0.000125, 'km', true, false), { met: 125, unit: 'mm' });
+
+    // step up
+    assert.deepEqual(stepUpOrDown(1e9, 'm', false, false), { met: 1e6, unit: 'km' });
+    assert.deepEqual(stepUpOrDown(1e9, 'm', false, true), { met: 1, unit: 'Gm' });
+    assert.deepEqual(stepUpOrDown(1e9, 'm', true, false), { met: 1e6, unit: 'km' });
+    assert.deepEqual(stepUpOrDown(1e9, 'm', true, true), { met: 1, unit: 'Gm' });
+    assert.deepEqual(stepUpOrDown(1e6, 'm', false, false), { met: 1e3, unit: 'km' });
+    assert.deepEqual(stepUpOrDown(1e6, 'm', false, true), { met: 1, unit: 'Mm' });
+    assert.deepEqual(stepUpOrDown(1e6, 'm', true, false), { met: 1e3, unit: 'km' });
+    assert.deepEqual(stepUpOrDown(1e6, 'm', true, true), { met: 1, unit: 'Mm' });
+
+    // returns 100.01 cm instead of 10.1 m
+    // assert.deepEqual(stepUpOrDown(10001, 'mm', false, false), { met: 10.1, unit: 'm' });
+    // returns 1000000 m instead of 1000 km
+    // assert.deepEqual(stepUpOrDown(1e8, 'cm', false, false), { met: 1000, unit: 'km' });
+}
+
 async function main() {
     testEvaluateFraction();
+    testStepUpOrDown();
 }
 
 main();
