f2cca4b634b6e44ce4b5a6ba0f6c8ae47a6643c8
Quentin Santos <qsantos@qsantos.fr>
2023-09-24 09:34:00 +0200

    Fix typing of evaluateFraction

--- lib.js
+++ lib.js
@@ -25,20 +25,23 @@ const fractions = {
  *  @return {number} - The value of the fraction (0 if evaluation fails)
 */
 function evaluateFraction(frac) {
-    if (fractions[frac] === undefined) {
-        try {
-            if (/[a-zA-Z,\?\!]/.test(frac))
-                return 0;
-            let cleanedFrac = frac.replace(/[^\d\/⁄]/, '');
-            cleanedFrac = frac.replace(/[⁄]/, '\/');
-            if (cleanedFrac.length < 3)
-                return 0;
-            return eval(cleanedFrac);
-        } catch (err) {
+    const value = fractions[frac];
+    if (value !== undefined) {
+        return value;
+    }
+    try {
+        if (/[a-zA-Z,\?\!]/.test(frac)) {
             return 0;
         }
+        let cleanedFrac = frac.replace(/[^\d\/⁄]/, '');
+        cleanedFrac = frac.replace(/[⁄]/, '\/');
+        if (cleanedFrac.length < 3) {
+            return 0;
+        }
+        return eval(cleanedFrac);
+    } catch (err) {
+        return 0;
     }
-    return fractions[frac];
 }
 
 /** If the value if smaller than 1, or larger than 10,000, use a more convenient SI prefix
