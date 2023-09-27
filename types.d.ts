06fe61e7f8ee4fc5b84a275fa3babb46f8ac8418
Quentin Santos <qsantos@qsantos.fr>
2023-09-27 18:50:08 +0200

    Document Conversion fields

--- types.d.ts
+++ types.d.ts
@@ -1,14 +1,14 @@
 export type Conversion = {
-    regexUnit?: RegExp,
-    regex?: RegExp,
-    unit: string,
-    unit2?: string,
-    multiplier: number,
-    multiplier2?: number,
-    multipliercu?: number,
-    multiplierimp?: number,
-    forceround?: boolean,
-    forceround2?: boolean,
+    regexUnit?: RegExp, // matching pattern for the unit only, when split over multiple text nodes (ParseUnitsOnly)
+    regex?: RegExp, // matching pattern
+    unit: string, // metric unit and SI prefix to use
+    unit2?: string, // alternate metric unit and SI prefix to use when useMM is set
+    multiplier: number, // conversion factor from matched unit to metric unit and SI prefix
+    multiplier2?: number, // conversion factor from matched unit to *alternate* metric unit andSI prefix
+    multipliercu?: number, // conversion factor to *litter* for volume (e.g. feet³)
+    multiplierimp?: number, // conversion factor when isUK is set (using imperial unit instead of US customary units)
+    forceround?: boolean, // whether to force rounding when the main unit is used
+    forceround2?: boolean, // whether to force rounding when the alternate unit is used
 };
 
 export type ValueWithUnit = {
